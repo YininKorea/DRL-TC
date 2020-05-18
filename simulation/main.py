2c0ab92b04cfe36546ef0f93f289597a43d3f59e
faibk <f.koegel@placity.de>
2020-05-18 16:32:26 +0900

    added evaluation and graphs

--- simulation/main.py
+++ simulation/main.py
@@ -2,19 +2,21 @@ from MCTS import MCTS, State
 from Model import DNN, Dataset
 from Simulation import Simulation
 
+import matplotlib.pyplot as plt
 import numpy as np
 import random
 
-n_nodes = 5
-n_iterations = 10
-n_episodes = 4
-n_searches = 10
+n_nodes = 20
+n_iterations = 40
+n_episodes = 10
+n_searches = 100
 exploration_level = 0.5
 
 def drltc():
 	training_dataset = Dataset()
 	dnn = DNN(n_nodes, minibatch=16, learning_rate=10e-6)
 	simulation = Simulation(n_nodes)
+	statistics = []
 
 	for iteration in range(n_iterations):
 		episode_root_state = State(np.zeros((n_nodes, n_nodes)))
@@ -42,15 +44,28 @@ def drltc():
 		dnn.train(training_dataset)
 
 		# construct test topology
-		# state = State(n_nodes)
-		# while not state.is_terminal():
-		# 	state_policy, _ = dnn.eval(state.adjacency)
-		# 	state_policy[~state.get_valid_actions()] = 0 # set invalid actions to 0
-		# 	state_policy /= state_policy.sum() # re-normalize over valid actions
-		# 	next_action = np.unravel_index(np.random.choice(n_nodes**2, p=state_policy.flatten()), shape=state_policy.shape)
-		# 	state = state.transition(next_action)
+		lifetimes = []
+		for i in range(10):
+			state = State(np.zeros((n_nodes, n_nodes)))
+			while not state.is_terminal():
+				state_policy, _ = dnn.eval(state.adjacency)
+				state_policy[~state.get_valid_actions()] = 0 # set invalid actions to 0
+				state_policy /= state_policy.sum() # re-normalize over valid actions
+				next_action = np.unravel_index(np.random.choice(n_nodes**2, p=state_policy.flatten()), shape=state_policy.shape)
+				state = state.transition(next_action)
 
-		# final_topology = state.adjacency
+			final_topology = state.adjacency
+			lifetimes.append(simulation.eval(final_topology))
+		statistics.append([sum(lifetimes)/10, max(lifetimes), min(lifetimes)])
+		print(f'statistics: {statistics[-1]}')
+
+	statistics = np.array(statistics)
+	plt.plot(statistics[:,0])
+	plt.plot(statistics[:,1])
+	plt.plot(statistics[:,2])
+	plt.ylabel('lifetime')
+	plt.xlabel('iteration')
+	plt.show()
 
 if __name__ == '__main__':
 	drltc()
