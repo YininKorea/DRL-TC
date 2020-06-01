fix distance calculation in simulation

simulation/simulation.py
@@ -20,7 +20,7 @@ class Simulation():
 		node_values = np.zeros(self.n_nodes)
 		self.aggregate(0, node_values, topology)
 		#dist_to_parent = np.array([self.dist(self.node_positions[n], self.node_positions[np.argmax(topology[:,n])]) for n in range(self.n_nodes)])
-		dist_to_parent = self.node_distances[np.argmax(topology, axis=-1)]
+		dist_to_parent = self.node_distances[range(self.n_nodes), np.argmax(topology, axis=0)]
 		energy_consumption = (self.rho * dist_to_parent**2 + self.e_p) * node_values
 		battery_lifetime = self.initial_energy / energy_consumption
 		battery_lifetime[0] = battery_lifetime.max() # make sure gateway is not the minimum (has theoretical infinite lifetime)